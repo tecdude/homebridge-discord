bd75be5b3f8707a35d036f2032850297e7448370
tecdude <fab_tec@icloud.com>
2022-06-18 15:36:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+var HttpWebHooksPlatform = require('./src/homekit/HttpWebHooksPlatform');
 var HttpWebHookSwitchAccessory = require('./src/homekit/accessories/HttpWebHookSwitchAccessory');
 var HttpWebHookPushButtonAccessory = require('./src/homekit/accessories/HttpWebHookPushButtonAccessory');
 var HttpWebHookStatelessSwitchAccessory = require('./src/homekit/accessories/HttpWebHookStatelessSwitchAccessory');
 
 module.exports = function(homebridge) {
-  homebridge.registerAccessory("homebridge-discord", "HttpWebHookSwitch", HttpWebHookSwitchAccessory);
-  homebridge.registerAccessory("homebridge-discord", "HttpWebHookPushButton", HttpWebHookPushButtonAccessory);
-homebridge.registerAccessory("homebridge-discord", "HttpWebHookStatelessSwitch", HttpWebHookStatelessSwitchAccessory);
+  homebridge.registerPlatform("homebridge-http-webhooks", "HttpWebHooks", HttpWebHooksPlatform);
+  homebridge.registerAccessory("homebridge-http-webhooks", "HttpWebHookSwitch", HttpWebHookSwitchAccessory);
+  homebridge.registerAccessory("homebridge-http-webhooks", "HttpWebHookPushButton", HttpWebHookPushButtonAccessory);
+  homebridge.registerAccessory("homebridge-http-webhooks", "HttpWebHookStatelessSwitch", HttpWebHookStatelessSwitchAccessory);
 };
